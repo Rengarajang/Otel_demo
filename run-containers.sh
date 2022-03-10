a7f26679e1e4e1881c214d04df11b9a33ed0ba28
root <root@ip-172-31-76-146.ec2.internal>
2022-03-10 14:15:57 +0000

    updated script

--- run-containers.sh
+++ run-containers.sh
@@ -3,7 +3,7 @@ echo "Building Docker images.."
 mvn clean package docker:build
 echo "Created Docker images.."
 docker images
-echo "Changing Permission for the mount folder.."
+echo "Changing Permission and update system config .."
 chmod -R 777 data
 sysctl -w vm.max_map_count=262144 
 echo "vm.max_map_count=262144" >> /etc/sysctl.conf
